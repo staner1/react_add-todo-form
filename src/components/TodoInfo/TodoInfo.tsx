b6aca6377a92adc22cf17af7de1fbd03b418c7a0
Oleksandr <kozyrev.sania@gmail.com>
2026-05-14 17:44:35 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,24 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { Todo } from '../../types/deal';
+import clsx from 'clsx';
+import { UserInfo } from '../UserInfo';
+import UsersFromServer from '../../api/users';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const currentUser = UsersFromServer.find(user => user.id === todo.userId);
+
+  return (
+    <article
+      data-id={todo.id}
+      className={clsx('TodoInfo', todo.completed && 'TodoInfo--completed')}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {currentUser !== undefined && <UserInfo user={currentUser} />}
+    </article>
+  );
+};
